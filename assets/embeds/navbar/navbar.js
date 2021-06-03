4afc55df6c15edb7787539d580a0dcbf23fe5408
CoolAbhi1290 <43380238+CoolAbhi1290@users.noreply.github.com>
2021-06-03 17:20:35 -0500

    j0321a

--- assets/embeds/navbar/navbar.js
+++ assets/embeds/navbar/navbar.js
@@ -1,25 +1,42 @@
-function windowOnload () {
+function windowOnload() {
     var searchButtonLink = document.getElementById("search-button-link");
     var searchBox = document.getElementById("search-box");
     var searchButton = document.getElementById("search-button");
-    
+
     searchButtonLink.removeAttribute("href");
     searchBox.style.display = "block";
-    
-    function attemptSearch (event) {
+
+    function attemptSearch(event) {
         if (searchBox.value.length > 0) {
             var query = encodeURIComponent(searchBox.value);
-            
-            // do something here lol
+            if (searchBox.offsetWidth > 10) {
+                alert(query);
+            }
         }
     }
-    
+
     searchButton.addEventListener("click", attemptSearch);
     searchBox.addEventListener("keyup", function (event) {
         if (event.keyCode == 13) {
             attemptSearch();
         }
     });
+
+    if (window.parent) {
+        var selectedMainUrlClass = "main-link-selected";
+        var paths = window.parent.location.pathname.split("/");
+        var mainLinks = document.getElementsByClassName("clickable-main-link");
+        var i = 0;
+        while (i != mainLinks.length) {
+            var targetUrl = mainLinks[i].attributes.getNamedItem("href").value.split("/");
+            if (targetUrl[1] == paths[1]) {
+                mainLinks[i].classList.add(selectedMainUrlClass);
+                break;
+            }
+
+            i++;
+        }
+    }
 }
 
 window.onload = windowOnload;
